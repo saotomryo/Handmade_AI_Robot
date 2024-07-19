caf1ab98157588cece3f72e80b4d8c0eeb4c6944
saotomeryo <ryosao@gmail.com>
2024-07-19 17:15:28 +0900

    会話履歴を保存して参照し続けるように変更

--- llms.py
+++ llms.py
@@ -2,8 +2,9 @@ import json
 from langchain_google_genai import ChatGoogleGenerativeAI
 #from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
+from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain.memory import ConversationBufferMemory
-from langchain_core.messages import HumanMessage
+from langchain.schema import AIMessage, HumanMessage
 # from langchain.prompts import ChatPromptTemplate,MessagesPlaceholder,SystemMessagePromptTemplate,HumanMessagePromptTemplate
 #from langchain.schema import messages_to_dict
 from langchain.prompts.chat import (
@@ -25,34 +26,26 @@ class llm_function():
                 HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
                 },temperature=0.7)
 
+        memory_path = "conversation_history.json"
+
+        # FileChatMessageHistoryインスタンスを作成
+        self.file_history = FileChatMessageHistory(memory_path)
+
         # 会話の履歴を保持するためのオブジェクト
         self.memory = ConversationBufferMemory(return_messages=True)
 
+        # 既存の履歴がある場合、メモリに読み込む
+        for message in self.file_history.messages:
+            if isinstance(message, HumanMessage):
+                self.memory.chat_memory.add_user_message(message.content)
+            elif isinstance(message, AIMessage):
+                self.memory.chat_memory.add_ai_message(message.content)
+
     def set_template(self):
 
-        # テンプレートを定義
-        self.template = f"""
-あなたは、子供用の玩具ロボットです。
-おしゃべりと会話内容に応じで、前に進むこと、後ろに下がること、写真を撮って質問に答えることができます。
-
-以下の手順で対応を行なってください。
-1.質問に対して、幼児向けに向けに非常に分かりやすい言葉で、50文字以内で返事を「返事」に記載する。
-2.質問に「こっちに来て」、「前に進む」に近い内容があれば「前進」に1を記載する
-3.質問に「あっち行って」、「後ろに行く」に近い内容があれば「後退」に1を記載します。
-4.質問に「右に回って」、「右をみて」など右に回る内容があれば「右」に1を記載します。
-5.質問に「左に回って」、「左をみて」など左に回る内容があれば「左」に1を記載します。
-6.質問内容で、映像を見ないと判断できないような内容がある場合は「撮影」に1を記載します。
-7.質問内容がうれしいと感じた時は「感情」に1を記載する
-
-出力形式はJSONで以下の項目で出力してください。
-返事：
-前進：
-後退：
-右：
-左：
-撮影：
-感情：
-        """
+        with open('prompt.txt', 'r', encoding='utf-8') as file:
+            # ファイル全体を一度に読み込む
+            self.template = file.read()
 
     def create_comversation(self):
 
@@ -74,7 +67,10 @@ class llm_function():
         command = f"{message}"
         response = self.conversation.predict(input=command)
 
-        print("test")
+        # 会話をファイルに保存
+        self.file_history.add_user_message(f"User:{message}")
+        self.file_history.add_ai_message(f"AI:{response}")
+
 
         return response
     
@@ -93,6 +89,10 @@ class llm_function():
 
         response = self.chat.invoke([message].content)
         print("回答:" + response)
+
+        # 会話をファイルに保存
+        self.file_history.add_user_message(f"User:{before_message}")
+        self.file_history.add_ai_message(f"AI:{response}")
 
         return response
 
